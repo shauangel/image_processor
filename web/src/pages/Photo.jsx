6445acf9bf4e4f69397e1069bb1039d2729c08fd
angelshau <shauangel2000@gmail.com>
2025-02-05 12:25:57 -0500

    UPDATE: frontend Photo page

--- web/src/pages/Photo.jsx
+++ web/src/pages/Photo.jsx
@@ -1,8 +1,14 @@
 import Image from 'react-bootstrap/Image';
+import FetchImage from "../components/FetchImage.jsx"
+import PhotoEditor from "../components/PhotoEditor.jsx"
 
 function Photo() {
   return (
-      <h1 className="container mt-5 pt-5 text-center text-2xl">Welcome to Photo Page</h1>
+      <>
+          <h1 className="container mt-5 pt-5 text-center text-2xl">Welcome to Photo Page</h1>
+          <FetchImage />
+          <PhotoEditor />
+      </>
       );
 }
 export default Photo;
